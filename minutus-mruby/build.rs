3b5246b8bf6fb2f866b393a6d5eac8618fc1b554
Yusuke Sangenya <longinus.eva@gmail.com>
2022-08-08 20:59:20 +0900

    Added CI

--- minutus-mruby/build.rs
+++ minutus-mruby/build.rs
@@ -6,15 +6,18 @@ fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=bridge");
     println!("cargo:rerun-if-changed=build.rs");
 
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let out_dir_str = env::var("OUT_DIR")?;
+    let out_dir = Path::new(&out_dir_str);
 
     download_mruby(&out_dir, &mruby_version()).unwrap();
     compile_bridge(&out_dir)?;
 
     if do_link() {
-        build_mruby(&out_dir);
+        build_mruby(&out_dir)?;
+        let search_dir = out_dir.join("mruby").join("build").join("host").join("lib");
+        ls_dir(&search_dir)?;
         println!("cargo:rustc-link-lib=mruby");
-        println!("cargo:rustc-link-search={}/mruby/build/host/lib", out_dir);
+        println!("cargo:rustc-link-search={}", search_dir.to_string_lossy());
     }
 
     println!("Finish build.rs");
@@ -22,13 +25,21 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn run_command(current_dir: &str, cmd: &[&str]) -> Result<()> {
+fn run_command(current_dir: &Path, cmd: &[&str]) -> Result<()> {
     println!("Start: {:?}", cmd);
 
-    let status = std::process::Command::new(cmd[0])
-        .args(&cmd[1..])
-        .current_dir(current_dir)
-        .status()?;
+    let status = if cfg!(target_os = "windows") {
+        std::process::Command::new("cmd")
+            .args(["/C", &cmd.join(" ")])
+            .current_dir(current_dir)
+            .status()?
+    } else {
+        std::process::Command::new(cmd[0])
+            .args(&cmd[1..])
+            .current_dir(current_dir)
+            .status()?
+    };
+
     if status.success() {
         Ok(())
     } else {
@@ -36,15 +47,27 @@ fn run_command(current_dir: &str, cmd: &[&str]) -> Result<()> {
     }
 }
 
-fn build_mruby(out_dir: &str) {
-    run_command(
-        Path::new(out_dir).join("mruby").to_str().unwrap(),
-        &["rake"],
-    )
-    .unwrap();
+fn build_mruby(out_dir: &Path) -> Result<()> {
+    run_command(&out_dir.join("mruby"), &["rake"])?;
+    if cfg!(target_os = "windows") {
+        let lib_dir = out_dir.join("mruby").join("build").join("host").join("lib");
+        std::fs::rename(lib_dir.join("libmruby.a"), lib_dir.join("mruby.lib"))?;
+    }
+    Ok(())
 }
 
-fn download_mruby(out_dir: &str, version: &str) -> Result<()> {
+fn download_mruby(out_dir: &Path, version: &str) -> Result<()> {
+    for d in &["mruby", &format!("mruby-{}", version)] {
+        let p = out_dir.join(d);
+        println!("Start removing {}", p.to_str().unwrap());
+        if p.exists() {
+            std::fs::remove_dir_all(p.clone())?;
+            println!("Finished removing {}", p.to_str().unwrap());
+        } else {
+            println!("Skip removing {}", p.to_str().unwrap());
+        }
+    }
+
     let url = if version == "master" {
         String::from("https://github.com/mruby/mruby/archive/refs/heads/master.tar.gz")
     } else {
@@ -53,13 +76,20 @@ fn download_mruby(out_dir: &str, version: &str) -> Result<()> {
             version
         )
     };
-    run_command(out_dir, &["wget", &url, "-O", "mruby.tar.gz"])?;
-    run_command(
-        out_dir,
-        &["rm", "-rf", &format!("mruby-{}", version), "mruby"],
+
+    let resp = reqwest::blocking::get(url)?;
+    let tar_gz = resp.bytes()?;
+    let tar = {
+        use bytes::Buf;
+        flate2::read::GzDecoder::new(tar_gz.reader())
+    };
+    let mut archive = tar::Archive::new(tar);
+    archive.unpack(out_dir)?;
+
+    std::fs::rename(
+        out_dir.join(format!("mruby-{}", version)),
+        out_dir.join("mruby"),
     )?;
-    run_command(out_dir, &["tar", "zxf", "mruby.tar.gz"])?;
-    run_command(out_dir, &["mv", &format!("mruby-{}", version), "mruby"])?;
 
     Ok(())
 }
@@ -84,11 +114,11 @@ fn mruby_version() -> String {
     return default.to_string();
 }
 
-fn compile_bridge(out_dir: &str) -> Result<()> {
-    let mruby_include_path = Path::new(out_dir).join("mruby/include");
+fn compile_bridge(out_dir: &Path) -> Result<()> {
+    let mruby_include_path = Path::new(out_dir).join("mruby").join("include");
     let out_path = Path::new(out_dir).join("mruby.rs");
 
-    run_command("bridge", &["ruby", "all.rb"])?;
+    run_command(&Path::new("bridge"), &["ruby", "all.rb"])?;
 
     let allowlist_types = &[
         "minu_.*",
@@ -115,7 +145,7 @@ fn compile_bridge(out_dir: &str) -> Result<()> {
 
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}", mruby_include_path.to_str().unwrap()))
-        .header("src/bridge.c")
+        .header(Path::new("src").join("bridge.c").to_string_lossy())
         .allowlist_type(allowlist_types.join("|"))
         .allowlist_function(allowlist_functions.join("|"))
         .layout_tests(false)
@@ -129,11 +159,21 @@ fn compile_bridge(out_dir: &str) -> Result<()> {
     println!("Start compiling binding");
 
     cc::Build::new()
-        .file("src/bridge.c")
+        .file(Path::new("src").join("bridge.c"))
         .include(mruby_include_path)
         .compile("minutus_bridge");
 
     println!("Finish compiling binding");
 
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn ls_dir(dir: &Path) -> Result<()> {
+    for entry in std::fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        println!("{:?}", path);
+    }
     Ok(())
 }
