42bb9a99ae8c55037a3d5af1789690bc793efb90
Yusuke Sangenya <longinus.eva@gmail.com>
2022-08-12 23:10:08 +0900

    shellscript -> Rakefile

--- examples/run_examples.sh
+++
@@ -1,7 +0,0 @@
-set -eux
-
-BASEDIR=$(dirname "$0")
-
-cd $BASEDIR/plane-mruby && cargo clean && cargo run
-cd $BASEDIR/custom-mruby && cargo clean && cargo run
-cd $BASEDIR/mruby-polars && rake clean test
