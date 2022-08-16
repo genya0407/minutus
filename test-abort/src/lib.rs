51cac319f9ff8d099a109c12a08523e8e2138100
Yusuke Sangenya <longinus.eva@gmail.com>
2022-08-16 20:03:42 +0900

    test

---
+++ test-abort/src/lib.rs
@@ -0,0 +1,80 @@
+#[test]
+fn test_load_string_exception() {
+    use minutus::mruby::*;
+
+    unsafe {
+        let mrb = minu_open();
+
+        // mrb.exc is null here
+        assert!((*mrb).exc.is_null());
+
+        // this does not cause panic nor abort
+        minu_load_string(mrb, "raise 'error!'\0".as_ptr() as _);
+
+        // mrb.exc is not null here
+        assert!(!(*mrb).exc.is_null());
+    }
+}
+
+#[test]
+fn test_funcall_exception() {
+    use minutus::mruby::*;
+
+    unsafe {
+        let mrb = minu_open();
+
+        // This causes exception, but does not panic nor abort
+        let result = minu_funcall(
+            mrb,
+            minu_obj_value((*mrb).top_self as _),
+            "nonexistent_method\0".as_ptr() as _,
+            0,
+        );
+        assert!(minu_exception_p(result));
+    }
+}
+
+#[test]
+fn test_raises_in_mrb() {
+    use minutus::mruby::*;
+
+    unsafe {
+        let mrb = minu_open();
+
+        if std::env::var("RUN_RAISE").is_ok() {
+            // This raises error, and aborts process
+            minu_raise(mrb, (*mrb).eStandardError_class, "errro!\0".as_ptr() as _);
+        }
+    }
+}
+
+/*
+$ cargo test
+   Compiling test-abort v0.1.0 (/Users/sangenya/dev/minutus/test-abort)
+    Finished test [unoptimized + debuginfo] target(s) in 0.38s
+     Running unittests src/lib.rs (/Users/sangenya/dev/minutus/target/debug/deps/test_abort-8f671ec3e349e0da)
+
+running 3 tests
+test test_raises_in_mrb ... ok
+test test_funcall_exception ... ok
+test test_load_string_exception ... ok
+
+test result: ok. 3 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+   Doc-tests test-abort
+
+running 0 tests
+
+test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
+
+$ RUN_RAISE=1 cargo test
+    Finished test [unoptimized + debuginfo] target(s) in 0.07s
+     Running unittests src/lib.rs (/Users/sangenya/dev/minutus/target/debug/deps/test_abort-8f671ec3e349e0da)
+
+running 3 tests
+errro! (StandardError)
+error: test failed, to rerun pass '--lib'
+
+Caused by:
+  process didn't exit successfully: `/Users/sangenya/dev/minutus/target/debug/deps/test_abort-8f671ec3e349e0da` (signal: 6, SIGABRT: process abort signal)
+ */
