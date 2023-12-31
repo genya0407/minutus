45f97ef6c75c52a4f976153229ec634c34caab26
Yusuke Sangenya <longinus.eva@gmail.com>
2023-12-31 22:41:28 +0900

    Added spec for 3.1.0 and 3.2.0

--- examples/custom-mruby-3_1_0/src/main.rs
+++ examples/custom-mruby-3_1_0/src/main.rs
@@ -1,6 +1,13 @@
 use minutus::types::*;
 use std::collections::HashMap;
 
+#[test]
+fn check_mruby_version() {
+    let evaluator = minutus::Evaluator::build();
+    let mruby_version = String::try_from_mrb(evaluator.evaluate("MRUBY_VERSION").unwrap()).unwrap();
+    assert_eq!(mruby_version, "3.1.0");
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let evaluator = minutus::Evaluator::build();
     let script = std::fs::read_to_string("some_script.rb").unwrap();
